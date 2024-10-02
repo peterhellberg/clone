1a4a70f98c7215fa0e9c7b82cb5fb2003e1d5e10
Peter Hellberg <peter@c7.se>
2024-10-02 19:03:32 +0200

    clone: Initial version

---
+++ main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+const repos = "Code/GitHub"
+
+type input struct {
+	base string
+	repo string
+	name string
+	link string
+	path string
+}
+
+func parseInput(args []string) (input, error) {
+	var in input
+
+	flags := flag.NewFlagSet(args[0], flag.ExitOnError)
+
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return in, err
+	}
+
+	defaultBase := filepath.Join(home, repos)
+
+	flags.StringVar(&in.base, "base", defaultBase,
+		"The base directory to clone into")
+
+	flags.StringVar(&in.name, "name", "",
+		"The name to use for the project, if blank use GitHub name")
+
+	if err := flags.Parse(args[1:]); err != nil {
+		return in, err
+	}
+
+	in.link = flags.Arg(0)
+
+	in.repo, err = extractRepo(in.link, in.name)
+	if err != nil {
+		return in, err
+	}
+
+	in.path = filepath.Join(in.base, in.repo)
+
+	return in, nil
+}
+
+func extractRepo(link, name string) (string, error) {
+	const (
+		gistPrefix = "git@gist.github.com:"
+		ghPrefix   = "git@github.com:"
+		suffix     = ".git"
+	)
+
+	repo := link
+
+	gist := strings.HasPrefix(repo, gistPrefix)
+
+	if !strings.HasPrefix(repo, ghPrefix) && !gist {
+		return "", fmt.Errorf("Missing %q prefix", ghPrefix)
+	}
+
+	repo = strings.TrimPrefix(repo, gistPrefix)
+	repo = strings.TrimPrefix(repo, ghPrefix)
+
+	if !strings.HasSuffix(link, suffix) {
+		return "", fmt.Errorf("Missing %q suffix", suffix)
+	}
+
+	repo = strings.TrimSuffix(repo, suffix)
+
+	var account, project string
+
+	if gist {
+		account = "Gists"
+		project = repo
+	} else {
+		var found bool
+
+		account, project, found = strings.Cut(repo, "/")
+		if !found {
+			return "", fmt.Errorf("Could not override repo name")
+		}
+	}
+
+	if name != "" {
+		project = name
+	}
+
+	return filepath.Join(account, project), nil
+}
+
+func main() {
+	if err := run(os.Args, os.Stderr); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
+	}
+}
+
+func run(args []string, stderr io.Writer) error {
+	in, err := parseInput(args)
+	if err != nil {
+		return err
+	}
+
+	cmd := exec.Command("git", "clone", in.link, in.path)
+
+	cmd.Stderr = stderr
+
+	return cmd.Run()
+}
---
+++ main_test.go
@@ -0,0 +1,50 @@
+package main
+
+import "testing"
+
+func TestExtractRepo(t *testing.T) {
+	t.Run("OK", func(t *testing.T) {
+		for _, tt := range []struct {
+			uri  string
+			gist string
+			repo string
+		}{
+			{
+				"git@github.com:peterhellberg/w4-init.git",
+				"",
+				"peterhellberg/w4-init",
+			},
+			{
+				"git@github.com:tsoding/zigout.git",
+				"",
+				"tsoding/zigout",
+			},
+			{
+				"git@github.com:tsoding/zigout.git",
+				"zigbreak",
+				"tsoding/zigbreak",
+			},
+			{
+				"git@gist.github.com:124fb025981b9b167f942c39b87e6624.git",
+				"",
+				"Gists/124fb025981b9b167f942c39b87e6624",
+			},
+			{
+				"git@gist.github.com:124fb025981b9b167f942c39b87e6624.git",
+				"aisnake",
+				"Gists/aisnake",
+			},
+		} {
+			t.Run(tt.uri, func(t *testing.T) {
+				repo, err := extractRepo(tt.uri, tt.gist)
+				if err != nil {
+					t.Fatalf("Unexpected error: %v", err)
+				}
+
+				if got, want := repo, tt.repo; got != want {
+					t.Fatalf("repo = %q, want %q", got, want)
+				}
+			})
+		}
+	})
+}
